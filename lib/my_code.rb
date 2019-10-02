7e99af8120243d35078d469fd1b3a719b024e7c3
Nicole M Darby <nicoledarby021322@gmail.com>
2019-10-02 13:31:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,8 +10,6 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
-  # My Code here....
-def map_to_negativize(source_array)
   final_array=[]
   counter= 0
   while counter < array.size do 
